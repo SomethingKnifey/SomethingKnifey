bd3eb35e87b158b7d9d1014b49fcd561af8a39e2
paulritzman <ritzman.paul@gmail.com>
2018-07-09 21:07:27 -0700

    Created scaffolding for MVC app; Set up Model and DbContext for Knife objects

---
+++ KnifeStore/Data/KnifeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnifeStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnifeStore.Data
+{
+    public class KnifeDbContext : DbContext
+    {
+        public KnifeDbContext(DbContextOptions<KnifeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Knife> Knives { get; set; }
+    }
+}
