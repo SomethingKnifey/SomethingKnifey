46543377cb43514c5b4a66f039853d0a7602e20b
Jesse Atay <jesse.atay@gmail.com>
2018-07-10 19:47:42 -0700

    updates prestaged for deployment

--- KnifeStore/Migrations/20180711024652_updatedModel.Designer.cs
+++ KnifeStore/Migrations/20180711024652_updatedModel.Designer.cs
@@ -4,14 +4,16 @@ using KnifeStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KnifeStore.Migrations
 {
     [DbContext(typeof(KnifeDbContext))]
-    partial class KnifeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180711024652_updatedModel")]
+    partial class updatedModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
