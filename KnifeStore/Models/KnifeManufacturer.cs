129d02dd6f135dc75b22a61369b7b6eded6f32dc
paulritzman <ritzman.paul@gmail.com>
2018-07-10 18:32:47 -0700

    Created a separate Knife Manufacturer model in order to link manufacturers to Knife objects

---
+++ KnifeStore/Models/KnifeManufacturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnifeStore.Models
+{
+    public class KnifeManufacturer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
