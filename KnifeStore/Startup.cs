f601da821497b29ffbcb74c5caa4195943d16d47
Jesse Atay <jesse.atay@gmail.com>
2018-07-10 19:42:20 -0700

    updates

--- KnifeStore/Startup.cs
+++ KnifeStore/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KnifeStore.Data;
+using KnifeStore.Models;
+using KnifeStore.Models.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +23,7 @@ namespace KnifeStore
 		{
 			Configuration = configuration;
 		}
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -29,6 +32,8 @@ namespace KnifeStore
 
 			services.AddDbContext<KnifeDbContext>(options => 
 			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddScoped<IInventory, InventoryActionModel>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
